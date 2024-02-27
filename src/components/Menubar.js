40e03bb267725bda684ff8b91299e686b9d72823
Indeshwar <indeshc@gmail.com>
2024-02-27 15:18:50 -0600

    Create Menubar component

---
+++ src/components/Menubar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { UseSelector, useDispatch, useSelector } from "react-redux";
+import { handleShowOffcanvas, handleCloseOffcanvas } from "../reduxfeature/showSlice";
+
+function Menubar() {
+  const show = useSelector((state)=>state.show.value);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <Nav className="justify-content-end flex-grow-1 pe-3">
+        <Nav.Link
+          as={Link}
+          to="/"
+          onClick={() => dispatch(handleCloseOffcanvas())}
+        >
+          Home
+        </Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/services"
+          onClick={() => dispatch(handleCloseOffcanvas())}
+        >
+          Services
+        </Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/portfolio"
+          onClick={() => dispatch(handleCloseOffcanvas())}
+        >
+          Portfolio
+        </Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/contact"
+          onClick={() => dispatch(handleCloseOffcanvas())}
+        >
+          Contact
+        </Nav.Link>
+      </Nav>
+    </>
+  );
+}
+
+export default Menubar;
