8093690e24e452012756b92e55b9bff68bb1abeb
Indeshwar <indeshc@gmail.com>
2024-03-03 22:56:29 -0600

    Add social media logo

--- src/components/FootBar.js
+++ src/components/FootBar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './style.css';
+import instagram from '../images/instagram.avif';
+import facebook from '../images/facebook.avif';
+import linkdin from '../images/linkdin.webp';
 function FootBar() {
   return (
     <footer>
@@ -8,9 +11,9 @@ function FootBar() {
                 <div className='col'>
                     <h2>Find me</h2>
                     <ul>
-                        <li >Facebook</li>
-                        <li>Instagram</li>
-                        <li>Linkdin</li>
+                        <li ><img className='social-media-frame' src={instagram} alt='Instagram'/></li>
+                        <li ><img className='social-media-frame' src={facebook} alt='Facebook'/></li>
+                        <li ><a href='https://www.linkedin.com/feed/'><img className='social-media-frame' src={linkdin} alt='Linkdin'/></a></li>
                     </ul>
                 </div>
             </div>
