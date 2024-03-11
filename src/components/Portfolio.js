3eb2e1e5a4a322a4a618f98d7b49237335dda3e9
Indeshwar <indeshc@gmail.com>
2024-03-10 23:09:20 -0500

    Add sample project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import Card from "./UI/Card";
+import './style.css';
+const Portfolio = () => {
+  const projects = useSelector((state) => state.show.projects);
 
-function Portfolio() {
+  const p = <ul>
+    {projects.map(p=><li key={p.id} className="main-content">
+      <h2>{p.title}</h2>
+      <div>
+        <span>{p.description}</span>
+      </div>
+    </li>) }
+  </ul>;
   return (
-    <div>Portfolio</div>
-  )
-}
+    <>
+      <>
+        {p}
+      </>
+    </>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
