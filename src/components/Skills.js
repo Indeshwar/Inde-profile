00e0b0b1ca9b72881d0b680d7cb5c7e5c66a0ac9
Indeshwar <indeshc@gmail.com>
2024-03-10 23:06:19 -0500

    Update the skills component by adding card component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,112 +1,31 @@
 import React from "react";
-import "./skills.css";
+import { useSelector } from "react-redux";
+import Card from "./UI/Card";
+import "./style.css";
 function Skills() {
-  return (
-    <>
-      <div className="container-fluid content">
-        <div className="row">
-          <div className="col">
-            <h1 className="title">My Skills</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <div>
-              <h2 className="title"> Programming Language</h2>
-              <ul className="ul">
-                <li>Java</li>
-                <li>Python</li>
-              </ul>
-            </div>
+  const skills = useSelector((state) => state.show.skills);
 
+  const skillList = (
+    <div className="container-fluid">
+      <ul className="row">
+        {skills.map((s) => (
+          <li key={s.id} className="col-lg-5 col-md-5 col-sm-12 main-content">
+            <h2>{s.title}</h2>
             <div>
-              <h2 className="title"> Database</h2>
-              <ul className="ul">
-                <li>JDBC</li>
-                <li>MYSQL</li>
-                <li>Postgress</li>
-                <li>Mongo DB</li>
-              </ul>
+              {s.name.map((n) => (
+                <li>{n}</li>
+              ))}
             </div>
-
-            <div>
-              <h2 className="title"> Front End</h2>
-              <ul className="ul">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Javascript</li>
-                <li>ReactJs</li>
-              </ul>
-            </div>
-
-            <div>
-              <h2 className="title"> IDEs</h2>
-              <ul className="ul">
-                <li>IntelliJ</li>
-                <li>Eclipse</li>
-              </ul>
-            </div>
-
-            <div>
-              <h2 className="title"> Cloud Technologies</h2>
-              <ul className="ul">
-                <li>Google Cloud Platform</li>
-                <li>Amazon Web Services</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col">
-            <div>
-              <h2 className="title"> Frameworks</h2>
-              <ul className="ul">
-                <li>Spring Boot</li>
-                <li>Spring Data JPA</li>
-                <li>Hibernate</li>
-              </ul>
-            </div>
-
-            <div>
-              <h2 className="title"> Version Control</h2>
-              <ul className="ul">
-                <li>Git</li>
-                <li>GitHub</li>
-              </ul>
-            </div>
-
-            <div>
-              <h2 className="title"> Continous Integration/Deployment</h2>
-              <ul className="ul">
-                <li>Jenkins</li>
-              </ul>
-            </div>
-
-            <div>
-              <h2 className="title"> Code Quality and Testing</h2>
-              <ul className="ul">
-                <li>SonarQube</li>
-                <li>Junit Test</li>
-              </ul>
-            </div>
-
-            <div>
-              <h2 className="title"> Containerization</h2>
-              <ul className="ul">
-                <li>Docker</li>
-              </ul>
-            </div>
-
-            <div>
-              <h2 className="title">Orchestration</h2>
-              <ul className="ul">
-                <li>Kubernetes</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
+
+  return <Card>
+    <h1 className="h1">Skills</h1>
+    {skillList}
+    </Card>;
 }
 
 export default Skills;
