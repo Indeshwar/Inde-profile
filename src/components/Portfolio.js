ea9a8b4f0c2996a86f6b3e3a4ece74f50ea81f77
Indeshwar <indeshc@gmail.com>
2024-03-12 16:08:28 -0500

    Update the component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,23 +1,34 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Card from "./UI/Card";
-import './style.css';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tabs';
+import "./style.css";
+import github from '../images/github.png'
 const Portfolio = () => {
   const projects = useSelector((state) => state.show.projects);
 
-  const p = <ul>
-    {projects.map(p=><li key={p.id} className="main-content">
-      <h2>{p.title}</h2>
-      <div>
-        <span>{p.description}</span>
-      </div>
-    </li>) }
-  </ul>;
+  const p = (
+    <div className="container-fluid">
+      <ul className="row">
+        {projects.map((p) => (
+          <li key={p.id} className="col-lg-8 col-sm-12 main-content">
+            <h2 className="title">{p.title}</h2>
+            <div>
+              <p>{p.description}</p>
+            </div>
+            <div>
+              <h3>GitHub</h3>
+              <a href= {p.link} className="d-inline-flex focus-ring focus-ring-success py-1 px-2 text-decoration-none border rounded-2"><img className="social-media-frame" src={github} alt="github"/></a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
   return (
     <>
-      <>
-        {p}
-      </>
+      <Card>{p}</Card>
     </>
   );
 };
