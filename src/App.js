5f4864797c6b13aacdb155af88bf01e147885952
Indeshwar <indeshc@gmail.com>
2024-03-12 16:04:35 -0500

    remove unnessecary import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import Header from './components/Header';
-import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/About';
 import Services from './components/Services';
