96f5023dc31d91d7998dfca823ee0ad16b91d0ba
Indeshwar <indeshc@gmail.com>
2024-03-19 15:46:28 -0500

    UPdate the Portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,25 +1,44 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Card from "./UI/Card";
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tabs';
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tabs";
 import "./style.css";
-import github from '../images/github.png'
+import github from "../images/github.png";
 const Portfolio = () => {
   const projects = useSelector((state) => state.show.projects);
 
   const p = (
     <div className="container-fluid">
-      <ul className="row">
+      <ul className="row ul">
         {projects.map((p) => (
-          <li key={p.id} className="col-lg-8 col-sm-12 main-content">
+          <li key={p.id} className="col-lg-11 col-sm-12 main-content li">
             <h2 className="title">{p.title}</h2>
             <div>
               <p>{p.description}</p>
             </div>
             <div>
-              <h3>GitHub</h3>
-              <a href= {p.link} className="d-inline-flex focus-ring focus-ring-success py-1 px-2 text-decoration-none border rounded-2"><img className="social-media-frame" src={github} alt="github"/></a>
+              <div className="row">
+                <div className="col-lg-10 col-md-12">
+                  <ul>
+                    {p.technologies.map((t)=> (<li className="content">{t}</li>) )}
+                  </ul>
+                 
+                </div>
+                <div className="col-lg-2">
+                  <h2>GitHub</h2>
+                  <a
+                    href={p.link}
+                    className="d-inline-flex focus-ring focus-ring-success py-1 px-2 text-decoration-none border rounded-2 content"
+                  >
+                    <img
+                      className="social-media-frame"
+                      src={github}
+                      alt="github"
+                    />
+                  </a>
+                </div>
+              </div>
             </div>
           </li>
         ))}
